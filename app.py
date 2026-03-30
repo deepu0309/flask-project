eae3c62912c258620691ab47dd944273415aef7d
deepu0309 <panwarkuldeepofficial@gmail.com>
2026-03-30 15:07:28 +0530

    Added backend API for todo submission

--- app.py
+++ app.py
@@ -45,6 +45,21 @@ def get_data():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo():
+    try:
+        item_name = request.form['itemName']
+        item_desc = request.form['itemDescription']
+
+        collection.insert_one({
+            "itemName": item_name,
+            "itemDescription": item_desc
+        })
+
+        return "To-Do item saved successfully"
+
+    except Exception as e:
+        return str(e)
 
 if __name__ == '__main__':
     app.run(debug=True)
